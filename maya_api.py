1c583ab8898856235d0d537f844d47022d21528d
spokenbk <spokenbk@gmail.com>
2025-05-02 21:35:10 -0300

    Add files via upload

---
+++ maya_api.py
@@ -0,0 +1,11 @@
+{
+  "application_id": null,
+  "avatar": "7a7c129f75c2d78d260dd358d7146c4e",
+  "channel_id": "1359976587679760597",
+  "guild_id": "524661433682755584",
+  "id": "1359976676959977502",
+  "name": "Captain Hook",
+  "type": 1,
+  "token": "vIwoveF2UWnffljuHG-5X5DBMxOG4vivrxgPDhbmybEvfdSCuWYk2gQEpQ1zmJmDdUlp",
+  "url": "DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/SEU_WEBHOOK"
+}
